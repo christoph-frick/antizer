4ec5c5d24601ec8188f8de68f5a7b9b5631d1b76
Christoph Frick <cf@ofnir.net>
2020-03-18 15:57:08 +0100

    Add an explicit releases deploy repo and force :gpg
    Somewhat failed to work as it used to be

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "https://github.com/christoph-frick/antizer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" {:password :gpg
+                                     :username :gpg
+                                     :url "https://clojars.org/repo/"}]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
                  [cljsjs/antd "3.26.12-0"]
